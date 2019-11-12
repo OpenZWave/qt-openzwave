f8e01864d8d5e9693137ce5313a6f91917762004
Justin Hammond <justin@dynam.ac>
2019-11-12 20:43:27 +0800

    forgot these files

---
+++ qt-ozwdaemon/mqttcommands/close.h
@@ -0,0 +1,17 @@
+#ifndef CLOSE_H
+#define CLOSE_H
+
+#include "mqttcommands/mqttcommands.h"
+
+class MqttCommand_Close : public MqttCommand {
+    Q_OBJECT
+public:
+    static MqttCommand *Create(QObject *parent = nullptr);
+    static QString StaticGetCommand() { return "Close";};
+    QString GetCommand() override { return StaticGetCommand(); };
+    bool processMessage(rapidjson::Document &) override;
+private: 
+    MqttCommand_Close(QObject *parent = nullptr);
+};
+
+#endif // CLOSE_H
