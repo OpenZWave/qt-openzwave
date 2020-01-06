b81fb249310fc2a9254942dcceff4c5a04250d64
Justin Hammond <justin@dynam.ac>
2020-01-06 22:47:58 +0800

    sentry upload debug files (#11)

--- tools/sentry-upload-debug.sh
+++ tools/sentry-upload-debug.sh
@@ -12,6 +12,16 @@ then
 	echo "Please Set the SENTRY_TOKEN enviroment variable"
 	exit -1
 fi
+#echo "deb http://deb.debian.org/debian-debug/ buster-debug main" > /etc/apt/sources.list.d/debug.list
+#echo "deb http://deb.debian.org/debian-debug/ buster-proposed-updates-debug main" >> /etc/apt/sources.list.d/debug.list
+#echo "deb http://deb.debian.org/debian-debug/ testing-debug main" >> /etc/apt/sources.list.d/debug.list
+#echo "deb http://deb.debian.org/debian-debug/ unstable-debug main" >> /etc/apt/sources.list.d/debug.list
+#echo "deb http://deb.debian.org/debian-debug/ experimental-debug main" >> /etc/apt/sources.list.d/debug.list
+apt-get update
+apt-get install -y curl
+if [ ! -f /usr/local/bin/sentry-cli ]; then curl -sL https://sentry.io/get-cli/ | bash; fi
+#apt-get install -t unstable libqt5core5a-dbgsym libqt5network5-dbgsym libqt5remoteobjects5-dbgsym
+#rm /etc/apt/sources.list.d/debug.list
 
 function findDBGFile() {
 	local DBGFILE
@@ -34,17 +44,17 @@ ldd $EXECUTABLE | awk '{print $3}' |
 				useable=`sentry-cli difutil check $lib | grep "Usable: yes"`
 				if [ ! -z "$useable" ]; then
 					RP=`realpath $lib`
-					DBGFILE=$(findDBGFile $lib)
-					if (( $? == 0)); then
-						LIBS+=" $DBGFILE"
-					fi
+					#DBGFILE=$(findDBGFile $lib)
+					#if (( $? == 0)); then
+					#	LIBS+=" $DBGFILE"
+					#fi
 					LIBS+=" $RP"
 					echo "Got Lib:" $RP " " $PKG " " $DBGFILE
 				fi
 			fi
 			
 		done
-		echo $LIBS
+		echo $LIBS /usr/local/bin/ozwdaemon
 		sentry-cli --auth-token $SENTRY_TOKEN upload-dif -o openzwave -p qt-openzwave $LIBS --wait
 	}
 
