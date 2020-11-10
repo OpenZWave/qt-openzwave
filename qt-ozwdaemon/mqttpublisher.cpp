4708939ef1e52d9248305fce7224cb7942634bbb
Olen <github@olen.net>
2020-11-10 15:26:06 +0100

    Increase keepalive
    This is a workaround for #140 to ensure the daemon is able to start up while we are waiting for a proper fix

--- qt-ozwdaemon/mqttpublisher.cpp
+++ qt-ozwdaemon/mqttpublisher.cpp
@@ -61,6 +61,7 @@ mqttpublisher::mqttpublisher(QSettings *settings, QObject *parent) :
     this->m_client->setHostname(settings->value("MQTTServer", "127.0.0.1").toString());
     this->m_client->setPort(static_cast<quint16>(settings->value("MQTTPort", 1883).toInt()));
     this->m_client->setClientId(QString("qt-openzwave-%1").arg(settings->value("Instance", 1).toInt()));
+    this->m_client->setKeepAlive(360);
     if (settings->contains("MQTTUsername")) {
         QString mqttpass = qgetenv("MQTT_PASSWORD");
     
